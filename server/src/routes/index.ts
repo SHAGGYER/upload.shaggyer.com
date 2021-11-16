e374ab1839abdf5a7459a2bd5a67b88b85cd505f
Mikolaj <Mikolaj Marciniak>
2021-11-16 06:41:40 +0100

    +

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,9 +1,11 @@
-import {Router} from "express";
+import express, {Router} from "express";
+import path from "path";
 
 import auth from "./auth";
 
 const router = Router();
 
 router.use("/api/auth", auth);
+router.use("/", express.static(path.join(__dirname, "../../../client/dist")));
 
 export default router;
