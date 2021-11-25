934a7a3227ca48e9dce5ee9b8c386dd112340924
Mikolaj <Mikolaj Marciniak>
2021-11-25 16:59:38 +0100

    +

--- server/src/services/AppService.js
+++ server/src/services/AppService.js
@@ -84,7 +84,8 @@ exports.AppService = class {
 
   installGithubRepo = (subdomain, {token, username, repo}) => {
     if (process.env.NODE_ENV === "dev") return false;
-    const serverCommand = `cd ${process.env.APPS_DIR} && curl -L -k -u ${token}:x-oauth-basic https://github.com/${username}/${repo}/tarball/master > ${repo}.gz`
+    let serverCommand = `cd ${process.env.APPS_DIR} && ./get-github-repo.sh ${token} ${username} ${repo}`;
+   /* const serverCommand = `cd ${process.env.APPS_DIR} && curl -L -k -u ${token}:x-oauth-basic https://github.com/${username}/${repo}/tarball/master > ${repo}.gz`*/
     const command = spawnSync(serverCommand, {shell: "/bin/bash"});
     if (command.stderr) {
       return command.stderr.toString();
