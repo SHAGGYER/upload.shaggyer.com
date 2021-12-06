1333cc56362be04c9c211feee2b91369b875763f
Mikolaj <Mikolaj Marciniak>
2021-12-06 04:05:09 +0100

    +

--- server/src/services/AppService.js
+++ server/src/services/AppService.js
@@ -104,7 +104,7 @@ exports.AppService = class {
     this.installGithubRepo(subdomain, {token, username, repo});
 
     const databaseName = subdomain.split("-").join("_");
-    const combinedName = repo;
+    const combinedName = repo + "-" + username + "-" + subdomain;
     const port = await getPort();
 
     let envArgs = [];
@@ -118,7 +118,7 @@ exports.AppService = class {
     const commands = [
       "FROM miko1991/miko-php:v1",
       `COPY ${combinedName}.gz .`,
-      `RUN sleep 1 && echo "Unpacking files..." && bsdtar --strip-components=1 -xvf ${combinedName}.gz -C .`,
+      `RUN sleep 1 && echo "Unpacking files..." && bsdtar --strip-components=1 -xvf ${subdomain}.gz -C .`,
       `RUN FILE=composer.json && if [ ! -e $FILE ]; then echo "File composer.json not found" && exit 3; fi;`,
       `RUN LARAVEL_FRAMEWORK=$(grep -m1 laravel/framework composer.json || echo "") && \
             if [ -z "$LARAVEL_FRAMEWORK" ]; \
