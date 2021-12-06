2313a92023b6cb5b93c07d638b510d48185ba1c6
Mikolaj <Mikolaj Marciniak>
2021-12-06 05:48:35 +0100

    +

--- server/src/services/AppService.js
+++ server/src/services/AppService.js
@@ -104,7 +104,7 @@ exports.AppService = class {
     socketServer,
     userId
   ) => {
-    const tarballName = await this.installGithubRepo(subdomain, {token, username, repo});
+    const tarballName = this.installGithubRepo(subdomain, {token, username, repo});
 
     const databaseName = subdomain.split("-").join("_");
     const port = await getPort();
